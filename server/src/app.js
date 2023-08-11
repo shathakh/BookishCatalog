70f389f7936a9637d9b9a14c6616477d15abe5a3
shatha Eqdaih <shathaqudaih17@gmail.com>
2023-08-11 09:16:45 +0300

    create scripts to access server and client

--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,10 @@ const cookieParser = require("cookie-parser");
 const compression = require("compression");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const cors = require('cors');
+const cors = require("cors");
 const { clientError, serverError } = require("./middlewares/error");
 const lumie = require("lumie");
 const path = require("path");
-// const ExpressWrapper = require("./helpers/expressWraper");
-// const { login, signup } = require("./controllers/authentication");
-// const { addBook, deleteBook, editBookInfo } = require("./controllers/book/book-post.action");
-// const authenticate = require("./middlewares/authenticate");
-// const { getBooks, searchBooks } = require("./controllers/book/book-get.action");
-
 
 dotenv.config();
 const app = express();
@@ -33,19 +27,10 @@ lumie.load(app, {
   preURL: "api",
   verbose: true,
   ignore: ["*.spec", "*.action"],
-  controllers_path: path.join(__dirname,"controllers")
+  controllers_path: path.join(__dirname, "controllers"),
 });
-
-// app.post('/login',ExpressWrapper(login))
-// app.post('/signup', ExpressWrapper(signup))
-// app.get('/book/:id', authenticate, ExpressWrapper(getBooks))
-// app.post('/book', authenticate, ExpressWrapper(addBook))
-// app.delete('/book/:id', authenticate, ExpressWrapper(deleteBook))
-// app.put('/book/:id', authenticate, ExpressWrapper(editBookInfo))
-// app.get('/book', authenticate, ExpressWrapper(searchBooks))
 
 app.use(clientError);
 app.use(serverError);
 
 module.exports = app;
-
